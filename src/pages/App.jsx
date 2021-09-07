eaff596a9d07fea45b924401cc70f48ab816b940
Gabriel Trindade <gabriel.m.trindade@gmail.com>
2021-09-07 20:42:36 -0300

    Project_React Palm

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -113,7 +113,7 @@ function UserList(){
                 <button onClick={()=>{modalResulOpen ()}}>Pagar</button>
             </div>
 
-            {/* Modal de recibo de pagamento */}
+            {/* -------------Modal de recibo de pagamento------------ */}
             <div className="modal" style={{display: resul}}>
                 <span>Recibo de pagamento</span>
                 <p>O Pagamento <strong>{paymentError}</strong> foi concluido com sucesso</p>
@@ -122,41 +122,4 @@ function UserList(){
       </>
     )
 }
-
-// Exportando o component Users
 export default UserList;
-/* import './UserList.css'
-import { useEffect, useState } from 'react'
-import axios from "axios"
-function UserList() {
-    // Função GET que está trazendo os dados da API
-    // useEffect, necessário para que o get execute apenas uma única vez
-    let [tarefas, setTarefas] = useState([])
-    useEffect(() => {
-        axios.get('https://www.mocky.io/v2/5d531c4f2e0000620081ddce', {
-            method: 'GET',
-        }).then((resp) => {setTarefas(resp.data)})
-    }, [])
-   // Retornando o conteúdo que será renderizado em tela
-   // Função map percorrendo todo o array recuperado anteriormente com o axios e listando na tela cada linha do array
-    return (
-        <>
-        {tarefas.map((t, index) => {
-            return (
-            <div className="user-container" key={'user'+index}>
-                <div className="user-wrapper">
-                    <img className="user-thumbnail" src={t.img} alt=""/>
-                    <div className="user-data">
-                        <p>Nome do Usuário {t.name}</p>
-                        <p>ID: {t.id} - Username: {t.username}</p>
-                    </div>
-                    <button>Pagar</button>
-                </div>
-            </div>
-            )
-        })}
-      </>
-    );
-}
-// Exportando o component Users
-export default UserList;*/
